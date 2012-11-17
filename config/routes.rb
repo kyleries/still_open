683828c9cdae9272138e7659bb59d371a2b00c08
Kyle Ries <kyle.ries@gmail.com>
2012-11-17 17:19:42 -0600

    Home markup complete, begin styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 StillOpen::Application.routes.draw do
+  
+  root :to => 'static_pages#home'
+  get "static_pages/home"
+
+  get "static_pages/about"
+
+  get "static_pages/contact"
+
+  get "static_pages/add"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
